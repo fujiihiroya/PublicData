fdd1243c352b5beead694257f0e55c44b6aadb15
fujiihiroya <130414828+fujiihiroya@users.noreply.github.com>
2023-04-14 13:28:12 +0900

    Add files via upload

---
+++ sample.c
@@ -0,0 +1,36 @@
+/*sample.c*/
+
+/* @wi GHT-509 */
+
+int DemoPolarion=1234;
+
+/* @wi GHT-509 */
+
+void test1(void)
+{
+	printf("Header reading\n");
+}
+
+int DemoPolarion=1234;
+
+void test2(void)
+{
+/* @wi GHT-509 */
+	printf("Header reading\n");
+}
+
+
+int DemoPolarion=1234;
+
+void test3(void)
+{
+	printf("Header reading\n");
+}
+
+
+int DemoPolarion=1234;
+
+void test4(void)
+{
+	printf("Header reading\n");
+}
